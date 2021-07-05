98e1216f327530496c5cb920182b84d410acfe91
Adrian Gette <adrian.gette@icloud.com>
2021-07-05 14:01:25 -0300

    Integrar scss

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { Button } from "semantic-ui-react";
 export default function App() {
   return (
     <div className="app">
-      <h1>Estamos en app</h1>
+      <h1>app</h1>
       <Button primary>Primary</Button>
       <Button secondary>Secondary</Button>
     </div>
