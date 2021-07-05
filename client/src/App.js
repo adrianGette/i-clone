ff6c9261bad3f006d5b3bd8893b95ddbad24c51f
Adrian Gette <adrian.gette@icloud.com>
2021-07-05 14:41:03 -0300

    Conectar app con server

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Button } from "semantic-ui-react";
+import { ApolloProvider } from '@apollo/client';
+import client from './config/apollo';
 
 
 export default function App() {
   return (
-    <div className="app">
-      <h1>app</h1>
-      <Button primary>Primary</Button>
-      <Button secondary>Secondary</Button>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="app">
+        <h1>app</h1>
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+      </div>
+    </ApolloProvider>
   );
 }
 
