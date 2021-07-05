3d4ba23d420b58cf9c8c1427dbc422837c79d4db
Adrian Gette <adrian.gette@icloud.com>
2021-07-05 13:07:26 -0300

    Integrar semantic-ui-react

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from "semantic-ui-react";
+
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Estamos en app</h1>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
+    </div>
+  );
+}
+
