5722863d5c868f40507e2433c39edc85724dd151
Adrian Gette <adrian.gette@icloud.com>
2021-07-05 15:09:35 -0300

    Separar zonas de auth e invitados

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Button } from "semantic-ui-react";
 import { ApolloProvider } from '@apollo/client';
 import client from './config/apollo';
+import Auth from './pages/Auth';
+
 
 
 export default function App() {
+
+  const [auth, setAuth] = useState({name: "Adrian"});
+
   return (
     <ApolloProvider client={client}>
-      <div className="app">
-        <h1>app</h1>
-        <Button primary>Primary</Button>
-        <Button secondary>Secondary</Button>
-      </div>
+      {!auth ? <Auth /> : <h1>Logueado</h1>}
     </ApolloProvider>
   );
 }
---
+++ client/src/pages/Auth/Auth.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Auth.scss";
+
+export default function Auth() {
+    return (
+        <div>
+            <h1>Esto es Auth</h1>
+        </div>
+    )
+}
