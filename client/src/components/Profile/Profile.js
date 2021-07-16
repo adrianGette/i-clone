9229e74bf2a6b8da87ace331555620f266cc6952
Adrian Gette <adrian.gette@icloud.com>
2021-07-16 15:32:58 -0300

    Implementar ModalBasic

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid, Image } from 'semantic-ui-react';
 import { useQuery } from '@apollo/client';
 import { GET_USER } from '../../gql/user';
 import UserNotFound from "../UserNotFound";
+import ModalBasic from "../Modal/ModalBasic";
 import imageNotFound from "../../assets/img/avatar.png";
 import "./Profile.scss";
 
 export default function Profile(props) {
 
     const { username } = props;
+
+    const [ showModal, setShowModal ] = useState(false);
 
     const { data, loading, error } = useQuery(GET_USER, {
         variables: { username },
@@ -25,7 +28,7 @@ export default function Profile(props) {
         <>
             <Grid className="profile">
                 <Grid.Column width={5} className="profile__left">
-                    <Image src={imageNotFound} avatar />
+                    <Image src={imageNotFound} avatar onClick={() => setShowModal(true)} />
                 </Grid.Column>
 
                 <Grid.Column width={11} className="profile__right">
@@ -44,6 +47,11 @@ export default function Profile(props) {
                     </div>
                 </Grid.Column>
             </Grid>
+            <ModalBasic show={showModal} setShow={setShowModal} title="Subir foto de perfil">
+                <p>Opciones...</p>
+                <p>Opciones...</p>
+                <p>Opciones...</p>               
+            </ModalBasic>
         </>
     );
 }
